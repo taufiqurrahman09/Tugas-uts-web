5a8eac339470a5cffc3746fe07069d6276f968ad
Taufiqurrahman <rtaufiq426@gmail.com>
2024-11-12 02:09:29 +0700

    saya memperbaiki code

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
-if($_SERVER["REQUEST_METHOD"]====="POST")
+if($_SERVER["REQUEST_METHOD"]=="POST")
 {
 
     //menangani data form yang dikirim 
     $name = htmlspecialchars($_POST["name"]);
-    $email = htmlspecialchars($_POST["pasword"];)
+    $password = htmlspecialchars($_POST["password"]);
     
     // cek jika ada data input kosong
-    if(empty($name)|| empty($email)){
+    if(empty($name)|| empty($password)){
         echo"Nama dan Pasword wajib diisi!";
 
 
@@ -15,7 +15,7 @@ if($_SERVER["REQUEST_METHOD"]====="POST")
         // Menampilkan hasil Pengolahan data
         echo"<h2>Terimakasih " .$name. "</h2>";
     
-        echo "<p>pasword sampean:" .$enail. "</p>";
+        echo "<p>pasword sampean:" .$password. "</p>";
     }
 
 }
